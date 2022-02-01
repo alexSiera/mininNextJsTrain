83f8e7ee6080780dd7a3335d1584a6f4a7865d51
devIntCool <dev@interactivecoolture.com>
2022-02-02 00:50:47 +0300

    Add getInitialProps implementation 1.49.00

--- next-js-cource/pages/post/[id].tsx
+++ next-js-cource/pages/post/[id].tsx
@@ -1,13 +1,36 @@
-import { useRouter } from "next/router";
+import { NextPage } from "next";
+import Link from "next/link";
 
 import { MainLayout } from "../../components/MainLayout";
 
-export default function Post() {
-  const router = useRouter();
-  console.log(router, "router");
+export type Post = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+type PostProps = {
+  post: Post;
+};
+
+const Post: NextPage<PostProps> = ({ post }) => {
   return (
     <MainLayout title="Single post">
-      <h1>Post: {router.query.id}</h1>
+      <h1>{post.title}</h1>
+      <hr />
+      <p>{post.body}</p>
+      <Link href="/posts">
+        <a>Back to all posts</a>
+      </Link>
     </MainLayout>
   );
-}
+};
+
+export default Post;
+
+Post.getInitialProps = async ({ query }) => {
+  const { id } = query;
+  const res = await fetch(`http://localhost:4200/posts/${id}`);
+  const post: Post = await res.json();
+  return { post };
+};
--- next-js-cource/pages/posts.tsx
+++ next-js-cource/pages/posts.tsx
@@ -1,17 +1,54 @@
-import Head from "next/head";
+import { NextPage } from "next";
+import Link from "next/link";
 
 import { MainLayout } from "../components/MainLayout";
+import { Post } from "./post/[id]";
 
-export default function Posts() {
+type PostsProps = {
+  posts: Post[];
+};
+
+const Posts: NextPage<PostsProps> = ({ posts }) => {
+  // const [posts, setPosts] = useState<Post[]>([]);
+
+  // Async loading data not working well with SSR
+  // useEffect(() => {
+  //   async function load() {
+  //     const data = await fetch("http://localhost:4200/posts");
+  //     const json: Post[] = await data.json();
+  //     setPosts(json);
+  //   }
+  //   load();
+  // }, []);
+
   return (
     <MainLayout>
       {/* <Head>
         <title>Post page next js cource</title>
       </Head> */}
       <h1>posts Page</h1>
+      <ul>
+        {posts.map(({ id, title, body }) => (
+          <li key={id}>
+            <Link as={`/post/${id}`} href={`/post/[id]`}>
+              <a>{title}</a>
+            </Link>
+            <p>{body}</p>
+          </li>
+        ))}
+      </ul>
     </MainLayout>
   );
-}
+};
+
+export default Posts;
+
+// This method is using for initial loading some data in a server...
+Posts.getInitialProps = async () => {
+  const data = await fetch("http://localhost:4200/posts");
+  const posts: Post[] = await data.json();
+  return { posts };
+};
 
 // /posts
 // /post/42
